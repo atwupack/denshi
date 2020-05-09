244a99aee523d15a6bf1f7b467d4c0916c3f36d9
André Twupack <atwupack@mailbox.org>
2020-05-09 18:37:04 +0200

    first implmentation of tree with static data only

--- examples/demo.rs
+++ examples/demo.rs
@@ -4,10 +4,28 @@ use denshi::component::text::TextField;
 use denshi::component::layout::{Form, Page, TabPane, Splitter, Orientation};
 use denshi::component::menu::MenuBar;
 use denshi::component::panel::Panel;
-use denshi::component::tree::Tree;
+use denshi::component::tree::{Tree, TreeNode};
 
 struct DemoState {
     text: String,
+}
+
+fn build_tree() -> Tree {
+    let mut tree = Tree::new();
+
+    let mut comp_node = TreeNode::new("Components");
+    comp_node.add_child(TreeNode::new("Buttons"));
+    tree.add_root(comp_node);
+
+    let mut cont_node = TreeNode::new("Containers");
+    cont_node.add_child(TreeNode::new("Forms"));
+    tree.add_root(cont_node);
+
+    let mut layout_node = TreeNode::new("Layouts");
+    layout_node.add_child(TreeNode::new("Page Layout"));
+    tree.add_root(layout_node);
+
+    tree
 }
 
 fn main() {
@@ -25,7 +43,7 @@ fn main() {
     // create tab pane
     let mut tabs = TabPane::new();
 
-    // first tab
+    // form tab
     let mut form = Form::new();
     let mut button = Button::new("Test Button".to_owned());
     button.set_click_event(|| {
@@ -49,11 +67,11 @@ fn main() {
     let mut right = Panel::new();
     right.set_title("Right".to_string());
 
-    let split = Splitter::new(Orientation::HORIZONTAL, left, right);
+    let split = Splitter::new(Orientation::VERTICAL, left, right);
     tabs.add_tab("Splitter".to_string(), split);
 
     // create tree
-    let tree = Tree::new();
+    let tree = build_tree();
 
     // create split pane
     let main_split = Splitter::new(Orientation::HORIZONTAL, tree, tabs);
--- src/component/tree.rs
+++ src/component/tree.rs
@@ -4,6 +4,38 @@ use crate::event::Event;
 
 pub struct TreeNode {
     caption: String,
+    nodes: Vec<TreeNode>,
+}
+
+impl TreeNode {
+    pub fn new(caption: impl Into<String>) -> Self {
+        TreeNode {
+            caption: caption.into(),
+            nodes: Vec::new(),
+        }
+    }
+
+    pub fn add_child(&mut self, node: TreeNode) {
+        self.nodes.push(node)
+    }
+
+    pub fn render_node(&self) -> String {
+        format!("<li data-caption=\"{caption}\" data-collapsed=\"true\">{children}</li>", caption = self.caption, children = self.render_children())
+    }
+
+    fn render_children(&self) -> String {
+        if self.nodes.is_empty() {
+            "".to_string()
+        }
+        else {
+            let mut s = "<ul>".to_string();
+            for node in &self.nodes {
+                s.push_str(node.render_node().as_str());
+            }
+            s.push_str("</ul>");
+            s
+        }
+    }
 }
 
 pub struct Tree {
@@ -18,11 +50,23 @@ impl Tree {
             roots: Vec::new(),
         }
     }
+
+    pub fn add_root(&mut self, node: TreeNode) {
+        self.roots.push(node)
+    }
+
+    fn render_roots(&self) -> String {
+        let mut s = String::new();
+        for root in &self.roots {
+            s.push_str(root.render_node().as_str());
+        }
+        s
+    }
 }
 
 impl Component for Tree {
     fn render(&self) -> String {
-        format!(r#"<ul id="{id}" data-role="treeview"><li data-caption="Test"></li > </ul>"#, id=self.id)
+        format!(r#"<ul id="{id}" data-role="treeview">{roots}</ul>"#, id=self.id, roots = self.render_roots())
     }
 
     fn handle_event(&mut self, event: &Event) {
